b9d12a5f0ac8cf60f767c4b79d8889748f07b6a7
khaled1018 <kh392828@gmail.com>
2025-03-11 07:10:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let intputNumbers = document.querySelectorAll(".otp-input");
+let proceedBtn = document.getElementsByTagName("button")[0];
+const popoverSuccess = document.getElementById("correct-otp");
+
+let correctOtp = "123456";
+let count = 0;
+let isProceeded = false;
+
+intputNumbers.forEach((input, index) => {
+  input.addEventListener("input", () => {
+    if (index !== intputNumbers.length - 1 && !isNaN(parseInt(input.value))) {
+      input.blur();
+      intputNumbers[index + 1].focus();
+    } else if (
+      index === intputNumbers.length - 1 &&
+      !isNaN(parseInt(input.value))
+    ) {
+      input.blur();
+    } else {
+      input.value = "";
+    }
+  });
+});
+
+proceedBtn.onclick = function () {
+  if (checkOtp() && !isProceeded) {
+    console.log("Yes");
+    isProceeded = true;
+    proceedBtn.setAttribute("popovertarget", popoverSuccess.id);
+  }
+};
+
+let checkOtp = () => {
+  for (let i = 0; i < correctOtp.length; i++) {
+    if (correctOtp[i] !== intputNumbers[i].value) {
+      return false;
+    }
+  }
+  return true;
+};
